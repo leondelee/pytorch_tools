f64bfe08d54b31079e2ff430d1aa24f10eca1e96
leondelee <30785200+leondelee@users.noreply.github.com>
2018-08-25 20:05:37 +0800

    Fix logical mistakes

--- monitor_email/email.sh
+++ monitor_email/email.sh
@@ -8,8 +8,9 @@ do
 avail=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
 
 # check if available memory is less or equal to 1.5GB
-if [[ "$avail" -le 30000000 && $send_flag == false ]]; then
+if [[ "$avail" -le 30000000 ]]; then
 
+if [[  $send_flag == false ]]; then
 # send email if system memory is running low
 /usr/sbin/ssmtp 302566290@qq.com</home/speit/cw-lab/monitor_email/alert_info.txt & xdotool key ctrl+d
 /usr/sbin/ssmtp 447626601@qq.com</home/speit/cw-lab/monitor_email/alert_info.txt & xdotool key ctrl+d
@@ -23,13 +24,14 @@ current_time=$( date +%s )
 
 # compute the interval between current time and last time
 interval=$(( ${current_time}-${last_time} ))
-
 # if the interval is bigger than 30 minutes, resend the email
 if [[ "$interval" -gt 60 ]]; then
 echo $interval
 last_time=$( date +%s ) > log.log
 send_flag=false
 echo 
+fi
+
 fi
 
 done
