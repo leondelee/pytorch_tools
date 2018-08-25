952ff38491ba15bd09cbc0a7363cf96382dd7cbc
speit <liliangwei@sjtu.edu.cn>
2018-08-25 17:34:03 +0800

    Monitoring and Alerting Script

---
+++ monitor_email/README.md
@@ -0,0 +1,2 @@
+# monitor_email
+This is a bash script which can monitor a ubuntu server's memory usage and send alerting email when necessary. 
--- monitor_email/email.sh
+++ monitor_email/email.sh
@@ -8,10 +8,11 @@ do
 avail=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
 
 # check if available memory is less or equal to 1.5GB
-if [[ "$avail" -le 10000000 && $send_flag == false ]]; then
+if [[ "$avail" -le 30000000 && $send_flag == false ]]; then
 
 # send email if system memory is running low
 /usr/sbin/ssmtp 302566290@qq.com</home/speit/cw-lab/monitor_email/alert_info.txt & xdotool key ctrl+d
+/usr/sbin/ssmtp 447626601@qq.com</home/speit/cw-lab/monitor_email/alert_info.txt & xdotool key ctrl+d
 send_flag=true
 echo last_time=$( date +%s ) > log.log
 fi
@@ -24,7 +25,7 @@ current_time=$( date +%s )
 interval=$(( ${current_time}-${last_time} ))
 
 # if the interval is bigger than 30 minutes, resend the email
-if [[ "$interval" -gt 1800 ]]; then
+if [[ "$interval" -gt 60 ]]; then
 echo $interval
 last_time=$( date +%s ) > log.log
 send_flag=false
